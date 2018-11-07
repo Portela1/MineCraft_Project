79ef952e55cc4f9f2250ec93aeb25f628b2f236a
manuelportela <manuelportela@192.168.0.13>
2018-11-07 14:52:19 -0400

    last commit for block

--- src/main/java/com/minecraft_megawallsrm/freeforall/util/handlers/RegistryHandler.java
+++ src/main/java/com/minecraft_megawallsrm/freeforall/util/handlers/RegistryHandler.java
@@ -1,8 +1,11 @@
 package com.minecraft_megawallsrm.freeforall.util.handlers;
 
+import com.minecraft_megawallsrm.freeforall.init.ModBlocks;
+//import com.minecraft_megawallsrm.freeforall.init.ModBlocks;
 import com.minecraft_megawallsrm.freeforall.init.ModItems;
 import com.minecraft_megawallsrm.freeforall.util.IHasModel;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
@@ -18,6 +21,12 @@ public class RegistryHandler {
 		event.getRegistry().registerAll(ModItems.ITEMS.toArray(new Item[0]));
 	}
 	
+	@SubscribeEvent
+	public static void onBlockRegister(RegistryEvent.Register<Block> event) {
+		
+		event.getRegistry().registerAll(ModBlocks.BLOCKS.toArray(new Block[0]));
+	}
+	
 	
 	@SubscribeEvent
 	public static void onModelRegister(ModelRegistryEvent event) {
@@ -26,6 +35,13 @@ public class RegistryHandler {
 			
 			if( item instanceof IHasModel) {
 				((IHasModel)item).registerModels();
+			}
+		}
+		
+		for(Block block : ModBlocks.BLOCKS) {
+			
+			if( block instanceof IHasModel) {
+				((IHasModel)block).registerModels();
 			}
 		}
 		
