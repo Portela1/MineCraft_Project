576235850fcd25e971aa0b4b567299bacd22c1e3
manuelportela <manuelportela@10.24.6.249>
2018-11-29 14:05:50 -0400

    Pending teleport to close block

--- src/main/java/com/minecraft_megawallsrm/freeforall/blocks/counter/BlockCounter.java
+++ src/main/java/com/minecraft_megawallsrm/freeforall/blocks/counter/BlockCounter.java
@@ -1,14 +1,20 @@
 package com.minecraft_megawallsrm.freeforall.blocks.counter;
 
+import java.util.ArrayList;
+
 import javax.annotation.Nullable;
 
 import com.minecraft_megawallsrm.freeforall.blocks.BlockTileEntity;
 import com.minecraft_megawallsrm.freeforall.comands.util.Teleport;
+import com.minecraft_megawallsrm.freeforall.util.IHasModel;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityCommandBlock;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
@@ -17,7 +23,7 @@ import net.minecraft.world.World;
 
 
 
-public class BlockCounter extends BlockTileEntity<TileEntityCounter> {
+public class BlockCounter extends BlockTileEntity<TileEntityCounter> implements IHasModel{
 
 	public BlockCounter() {
 		super(Material.DRAGON_EGG, "counter"); 
@@ -30,7 +36,8 @@ public class BlockCounter extends BlockTileEntity<TileEntityCounter> {
 			if (side == EnumFacing.DOWN) {
 				tile.decrementCount();}
 			else if (side == EnumFacing.NORTH || side == EnumFacing.SOUTH || side == EnumFacing.EAST|| side == EnumFacing.WEST) {
-				Teleport.teleportToDemension(player, player.getPosition().getX()+20, player.getPosition().getY()+20, player.getPosition().getZ()+20);
+				BlockPos closest = closestblock(world,pos,8);
+				Teleport.teleportToDemension(player, closest.getX(), closest.getY(),closest.getZ());
 			
 			} else if (side == EnumFacing.UP) {
 				tile.incrementCount();
@@ -38,6 +45,28 @@ public class BlockCounter extends BlockTileEntity<TileEntityCounter> {
 			player.sendMessage(new TextComponentString("ID: " + tile.getCount()));
 		}
 		return true;
+	}
+	
+	
+	static BlockPos closestblock(World world, BlockPos start, int rad){
+		int counter = 0;
+	while(counter !=rad ) {
+		for(int x = start.getX()-counter; x<=start.getX()+counter;x++)
+			for(int y = start.getY()-counter; y <= start.getY()+counter;y++)
+				for(int z = start.getZ()-counter; z <= start.getZ()+counter;z++) {
+					BlockPos position = new BlockPos(x,y,z);
+					if((position.getX() - start.getX() < 1 && position.getY()-start.getY()<1 && position.getZ()-start.getZ()<1)) {
+						continue;
+					}
+					
+					if (world.getTileEntity(position) instanceof TileEntityCounter){
+						return position;
+					}	
+				}
+		counter++;
+	}
+	return new BlockPos(start.getX(),start.getY(),start.getZ());
+		
 	}
 	
 	@Override
--- src/main/java/com/minecraft_megawallsrm/freeforall/blocks/counter/TileEntityCounter.java
+++ src/main/java/com/minecraft_megawallsrm/freeforall/blocks/counter/TileEntityCounter.java
@@ -1,11 +1,17 @@
 package com.minecraft_megawallsrm.freeforall.blocks.counter;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
 
-public class TileEntityCounter extends TileEntity {
+public class TileEntityCounter extends TileEntity implements ITickable, IInventory {
 
 	private int count;
+	private ItemStack[] inventory;
+	public int usagesLeft;
 
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
@@ -31,6 +37,114 @@ public class TileEntityCounter extends TileEntity {
 	public void decrementCount() {
 		count--;
 		markDirty();
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasCustomName() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getSizeInventory() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ItemStack decrStackSize(int index, int count) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ItemStack removeStackFromSlot(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setInventorySlotContents(int index, ItemStack stack) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getInventoryStackLimit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isUsableByPlayer(EntityPlayer player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void openInventory(EntityPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeInventory(EntityPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isItemValidForSlot(int index, ItemStack stack) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getField(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setField(int id, int value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getFieldCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
